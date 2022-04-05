3f94ae240e8635cf3e399899d0a0652d88ffb0c2
Patryk Pastor <patryk.pastor@cstx-es.de>
2022-04-05 11:29:20 +0200

    Added profiles viewset

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,6 +1,40 @@
 from rest_framework import serializers
 
+from profiles_api import models
+
 
 class HelloSerializer(serializers.Serializer):
     """Serializes a name field for testing our APIView"""
     name = serializers.CharField(max_length=10)
+
+
+class UserProfileSerializer(serializers.ModelSerializer): #podobny do basic ale ma duzo funkcjonalnosci ktore pomagaja
+    """Serializes a user profile object"""                 #obslugiwac django rest framework
+
+    class Meta: # zawsze w modelserializer konfigurujemy zeby wskazywal na szczegolny model w naszym projekcie
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')        #lista pol w naszym modelu ktore chcemy obslugiwac przez serializer
+        extra_kwargs = {
+            'password': {       #nie chcemy zeby haslo bylo nadpisane i retrieve. Tutaj pokazujemy ze chcemy wlasna konfiguracje do password
+                'write_only': True,      #mozemy uzyc tylko do create i update, bez get
+                'style': {'input_type': 'password'}         #gwiazdki albo kropki zamiast wpisywanego hasla
+            }
+        }
+
+    def create(self, validated_data):
+        """Create and return a new user"""
+        user = models.UserProfile.objects.create_user( #uzywa metody create_user z models UserProfilManager i tam jest setPassword dlatego
+            email=validated_data['email'],              #bedzie podawane wczesniej i ustawiane jako hash, nie bedzie widoczne w programie
+            name=validated_data['name'],
+            password=validated_data['password']
+        )
+
+        return user
+
+    def update(self, instance, validated_data):
+        """Handle updating user account"""
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+
+        return super().update(instance, validated_data)
--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -5,6 +5,7 @@ from profiles_api import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset') #w ViewSet bedzie widoczne tylko to co jest dodane do routera i nic wiecej
+router.register('profile', views.UserProfileViewSet) #nie trzeba base_name poniewaz mamy w ModelViewSet queryset object. DRF daje imie od razu
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()), #to nie bedzie widoczne
